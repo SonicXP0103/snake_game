1baa7ac912f6789753c44ccb1f3c2d14edaaed9c
sonic <sonic0103code@gmail.com>
2025-07-25 01:03:20 +0800

    [修改] 搬移Vue架構

--- script.js
+++ script.js
@@ -367,7 +367,6 @@ function endGame()
     }
 }
 
-/* 改 Vue.js 版
 function resizeCanvasIfMobile()
 {
     const canvas = document.getElementById("gameCanvas");
@@ -399,7 +398,6 @@ function resizeCanvasIfMobile()
         canvas.height = 400;
     }
 }
-*/
 
 // #endregion 自訂義函數
 
@@ -420,8 +418,6 @@ function UpdateScoreUI()
 
 // ===== 主要邏輯 =====
 
-/* 改 Vue 的 mounted
-
 // 取得 canvas 與畫布內容
 const canvas = document.getElementById("gameCanvas");
 const ctx    = canvas.getContext("2d");
@@ -431,7 +427,6 @@ resizeCanvasIfMobile();
 
 // 螢幕尺寸變化時也要調整
 window.addEventListener("resize", resizeCanvasIfMobile);
-*/
 
 // 監聽鍵盤事件
 document.addEventListener("keydown", function(e)
